338327c2404e966ae57fa62332fcb41387053955
mribrahimkhan360 <mribrahimkhan360@gmail.com>
2026-04-23 20:57:32 +0600

    git push database

--- elms.sql
+++ elms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2026 at 12:10 PM
+-- Generation Time: Apr 23, 2026 at 04:56 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,7 +40,7 @@ CREATE TABLE `administrations` (
 --
 
 INSERT INTO `administrations` (`id`, `sick`, `casual`, `created_at`, `updated_at`) VALUES
-(1, 30, 30, '2026-04-23 02:07:12', '2026-04-23 02:07:12');
+(1, 0, 0, '2026-04-23 02:07:12', '2026-04-23 02:07:12');
 
 -- --------------------------------------------------------
 
@@ -59,7 +59,7 @@ CREATE TABLE `cache` (
 --
 
 INSERT INTO `cache` (`key`, `value`, `expiration`) VALUES
-('spatie.permission.cache', 'a:3:{s:5:\"alias\";a:4:{s:1:\"a\";s:2:\"id\";s:1:\"b\";s:4:\"name\";s:1:\"c\";s:10:\"guard_name\";s:1:\"r\";s:5:\"roles\";}s:11:\"permissions\";a:7:{i:0;a:3:{s:1:\"a\";i:4;s:1:\"b\";s:4:\"edit\";s:1:\"c\";s:3:\"web\";}i:1;a:3:{s:1:\"a\";i:5;s:1:\"b\";s:6:\"delete\";s:1:\"c\";s:3:\"web\";}i:2;a:4:{s:1:\"a\";i:6;s:1:\"b\";s:9:\"admin_nav\";s:1:\"c\";s:3:\"web\";s:1:\"r\";a:1:{i:0;i:2;}}i:3;a:4:{s:1:\"a\";i:7;s:1:\"b\";s:18:\"nav_administration\";s:1:\"c\";s:3:\"web\";s:1:\"r\";a:2:{i:0;i:2;i:1;i:3;}}i:4;a:4:{s:1:\"a\";i:8;s:1:\"b\";s:22:\"nav_nav_administration\";s:1:\"c\";s:3:\"web\";s:1:\"r\";a:2:{i:0;i:2;i:1;i:3;}}i:5;a:4:{s:1:\"a\";i:9;s:1:\"b\";s:14:\"delete_account\";s:1:\"c\";s:3:\"web\";s:1:\"r\";a:1:{i:0;i:3;}}i:6;a:3:{s:1:\"a\";i:10;s:1:\"b\";s:18:\"leave_approval_btn\";s:1:\"c\";s:3:\"web\";}}s:5:\"roles\";a:2:{i:0;a:3:{s:1:\"a\";i:2;s:1:\"b\";s:5:\"Admin\";s:1:\"c\";s:3:\"web\";}i:1;a:3:{s:1:\"a\";i:3;s:1:\"b\";s:2:\"Hr\";s:1:\"c\";s:3:\"web\";}}}', 1777025402);
+('spatie.permission.cache', 'a:3:{s:5:\"alias\";a:4:{s:1:\"a\";s:2:\"id\";s:1:\"b\";s:4:\"name\";s:1:\"c\";s:10:\"guard_name\";s:1:\"r\";s:5:\"roles\";}s:11:\"permissions\";a:10:{i:0;a:3:{s:1:\"a\";i:4;s:1:\"b\";s:4:\"edit\";s:1:\"c\";s:3:\"web\";}i:1;a:3:{s:1:\"a\";i:5;s:1:\"b\";s:6:\"delete\";s:1:\"c\";s:3:\"web\";}i:2;a:4:{s:1:\"a\";i:6;s:1:\"b\";s:9:\"admin_nav\";s:1:\"c\";s:3:\"web\";s:1:\"r\";a:1:{i:0;i:2;}}i:3;a:4:{s:1:\"a\";i:7;s:1:\"b\";s:18:\"nav_administration\";s:1:\"c\";s:3:\"web\";s:1:\"r\";a:2:{i:0;i:2;i:1;i:3;}}i:4;a:4:{s:1:\"a\";i:8;s:1:\"b\";s:22:\"nav_nav_administration\";s:1:\"c\";s:3:\"web\";s:1:\"r\";a:2:{i:0;i:2;i:1;i:3;}}i:5;a:3:{s:1:\"a\";i:9;s:1:\"b\";s:14:\"delete_account\";s:1:\"c\";s:3:\"web\";}i:6;a:4:{s:1:\"a\";i:10;s:1:\"b\";s:18:\"leave_approval_btn\";s:1:\"c\";s:3:\"web\";s:1:\"r\";a:1:{i:0;i:3;}}i:7;a:4:{s:1:\"a\";i:11;s:1:\"b\";s:15:\"apply_leave_btn\";s:1:\"c\";s:3:\"web\";s:1:\"r\";a:1:{i:0;i:6;}}i:8;a:4:{s:1:\"a\";i:13;s:1:\"b\";s:25:\"create_annually_leave_btn\";s:1:\"c\";s:3:\"web\";s:1:\"r\";a:1:{i:0;i:2;}}i:9;a:4:{s:1:\"a\";i:14;s:1:\"b\";s:23:\"configure_action_button\";s:1:\"c\";s:3:\"web\";s:1:\"r\";a:1:{i:0;i:2;}}}s:5:\"roles\";a:3:{i:0;a:3:{s:1:\"a\";i:2;s:1:\"b\";s:5:\"Admin\";s:1:\"c\";s:3:\"web\";}i:1;a:3:{s:1:\"a\";i:3;s:1:\"b\";s:2:\"Hr\";s:1:\"c\";s:3:\"web\";}i:2;a:3:{s:1:\"a\";i:6;s:1:\"b\";s:8:\"employee\";s:1:\"c\";s:3:\"web\";}}}', 1777038577);
 
 -- --------------------------------------------------------
 
@@ -137,7 +137,7 @@ CREATE TABLE `leaves` (
   `to_date` date NOT NULL,
   `leave_type` enum('sick','casual') NOT NULL,
   `attachment` varchar(255) DEFAULT NULL,
-  `approved_by` bigint(20) UNSIGNED DEFAULT NULL,
+  `status` enum('pending','approved','rejected') NOT NULL DEFAULT 'pending',
   `approved_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -148,10 +148,10 @@ CREATE TABLE `leaves` (
 -- Dumping data for table `leaves`
 --
 
-INSERT INTO `leaves` (`id`, `user_id`, `form_date`, `to_date`, `leave_type`, `attachment`, `approved_by`, `approved_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 1, '2026-04-23', '2026-04-24', 'sick', 'Permissions/BbxlSZYy7PJikDQCStnPJDVt9bsifhPV6h9WdyLH.png', NULL, NULL, '2026-04-23 02:29:13', '2026-04-23 03:57:48', '2026-04-23 03:57:48'),
-(2, 1, '2026-04-24', '2026-04-25', 'sick', 'Permissions/oYkz8qbF0cM2O6ddjGTho3WwdGeiDxGkEBc4F5ZI.png', NULL, NULL, '2026-04-23 03:56:29', '2026-04-23 03:57:51', '2026-04-23 03:57:51'),
-(3, 1, '2026-04-23', '2026-04-24', 'casual', 'image/1pH79hxQU14vwNGAyv94p8YsHarwyx9g1DDVTY9o.png', NULL, NULL, '2026-04-23 03:58:57', '2026-04-23 03:58:57', NULL);
+INSERT INTO `leaves` (`id`, `user_id`, `form_date`, `to_date`, `leave_type`, `attachment`, `status`, `approved_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 3, '2026-04-23', '2026-04-24', 'sick', 'images/OrU0Zv4kDuheBMw28WNuK3u0nxNQaj1g1djtGcep.png', 'approved', '2026-04-23 08:38:08', '2026-04-23 05:27:52', '2026-04-23 08:38:08', NULL),
+(2, 3, '2026-04-22', '2026-04-30', 'sick', 'images/u8LKzjabYPDnAoi9JA8z3LRJ8AWeReZrAgAF2You.png', 'rejected', '2026-04-23 05:59:18', '2026-04-23 05:32:23', '2026-04-23 05:59:18', NULL),
+(3, 4, '2026-04-23', '2026-04-24', 'sick', 'images/f7VY61KreI9qmblNZnehR8De35PSGQ66RG9xFDdH.png', 'pending', NULL, '2026-04-23 07:14:36', '2026-04-23 07:14:36', NULL);
 
 -- --------------------------------------------------------
 
@@ -175,7 +175,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (9, '0001_01_01_000001_create_cache_table', 2),
 (10, '0001_01_01_000002_create_jobs_table', 2),
 (11, '2026_04_22_093001_create_administrations_table', 3),
-(12, '2026_04_23_050532_create_leaves_table', 4);
+(13, '2026_04_23_050532_create_leaves_table', 4);
 
 -- --------------------------------------------------------
 
@@ -247,7 +247,10 @@ INSERT INTO `permissions` (`id`, `name`, `guard_name`, `created_at`, `updated_at
 (7, 'nav_administration', 'web', '2026-04-22 02:54:26', '2026-04-22 02:54:26'),
 (8, 'nav_nav_administration', 'web', '2026-04-22 03:07:48', '2026-04-22 03:07:48'),
 (9, 'delete_account', 'web', '2026-04-22 03:16:08', '2026-04-22 03:16:08'),
-(10, 'leave_approval_btn', 'web', '2026-04-23 04:03:44', '2026-04-23 04:03:44');
+(10, 'leave_approval_btn', 'web', '2026-04-23 04:03:44', '2026-04-23 04:03:44'),
+(11, 'apply_leave_btn', 'web', '2026-04-23 06:14:00', '2026-04-23 06:14:00'),
+(13, 'create_annually_leave_btn', 'web', '2026-04-23 06:26:36', '2026-04-23 06:26:36'),
+(14, 'configure_action_button', 'web', '2026-04-23 06:31:12', '2026-04-23 06:31:12');
 
 -- --------------------------------------------------------
 
@@ -293,7 +296,10 @@ INSERT INTO `role_has_permissions` (`permission_id`, `role_id`) VALUES
 (7, 3),
 (8, 2),
 (8, 3),
-(9, 3);
+(10, 3),
+(11, 6),
+(13, 2),
+(14, 2);
 
 -- --------------------------------------------------------
 
@@ -315,7 +321,8 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('zWjVTlBvKFT6Avcr1wnSglT6KGqfu9zGbkIk97Yq', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiUEE3ZUJpalo5dlhFY1JKSVliWkxxYlFaa3I2SlFHREY4Tnd3OGRMbiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9yb2xlcyI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1776939002);
+('F1UCtwyaJzXnWuwJKg1ilU9dLznJl5JW3ZoBhLDu', 3, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:149.0) Gecko/20100101 Firefox/149.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiRU92d2NtVlB3MERIbmRGbFpZY2NWSllVN0h2YXN6RGszbGZrYkJBUCI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sZWF2ZSI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjM7fQ==', 1776955116),
+('Y70qFdChGLrEuY0wzFGXzRqQ1gIEjPBnuyxqefoZ', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiMHhiclpVQUR6amNTTFR2WEE3SWFNcmtreXpmdG9jZDhnNDlxaFZEbyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbmlzdHJhdGlvbnMiO31zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO30=', 1776954764);
 
 -- --------------------------------------------------------
 
@@ -340,8 +347,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Ibrahim Khan', 'mribrahimkhan360@gmail.com', NULL, '$2y$12$RnlFCqJbBI7jyCWNBdoC2.p9UgQgSv6sJyWMOgmfPHtld.Coi7znS', NULL, '2026-04-20 22:51:48', '2026-04-22 02:44:08'),
-(3, 'saor', 'sagor@gmail.com', NULL, '$2y$12$UmDfUqyg.5HHKAXyQJ6Cv.J7.2R0vqTgihOl.L8TerVDQ.5CbUaIS', NULL, '2026-04-22 02:31:42', '2026-04-22 02:31:42'),
-(4, 'rana', 'rana@gmail.com', NULL, '$2y$12$QIXQJ9hipUMMAEPfST67ieo1UX4NJukk4FNl4/8ExNbndCR6T/WeW', 'j9IrHOnDmWYPjfChCKtZmzQSrlGmTQ5QxWR2yoplxn1LbYtQh2Mw6kvJpwxr', '2026-04-22 02:32:15', '2026-04-22 02:32:15');
+(3, 'saor', 'sagor@gmail.com', NULL, '$2y$12$UmDfUqyg.5HHKAXyQJ6Cv.J7.2R0vqTgihOl.L8TerVDQ.5CbUaIS', 'cy9CnN2FOLFNizZUX84a3fAGxZQ2u0GsUAJbtAPtclmcuHuhARFA49cyxF6w', '2026-04-22 02:31:42', '2026-04-22 02:31:42'),
+(4, 'rana', 'rana@gmail.com', NULL, '$2y$12$QIXQJ9hipUMMAEPfST67ieo1UX4NJukk4FNl4/8ExNbndCR6T/WeW', 'yHAarEpxpbteAE7X58P6vSDjUlKVrA5rhDTzl00Wgex44r08VFnZw955dOiu', '2026-04-22 02:32:15', '2026-04-22 02:32:15');
 
 --
 -- Indexes for dumped tables
@@ -390,8 +397,7 @@ ALTER TABLE `job_batches`
 --
 ALTER TABLE `leaves`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `leaves_user_id_foreign` (`user_id`),
-  ADD KEY `leaves_approved_by_foreign` (`approved_by`);
+  ADD KEY `leaves_user_id_foreign` (`user_id`);
 
 --
 -- Indexes for table `migrations`
@@ -487,13 +493,13 @@ ALTER TABLE `leaves`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `permissions`
 --
 ALTER TABLE `permissions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `roles`
@@ -515,7 +521,6 @@ ALTER TABLE `users`
 -- Constraints for table `leaves`
 --
 ALTER TABLE `leaves`
-  ADD CONSTRAINT `leaves_approved_by_foreign` FOREIGN KEY (`approved_by`) REFERENCES `users` (`id`) ON DELETE SET NULL,
   ADD CONSTRAINT `leaves_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
